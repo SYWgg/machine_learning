a52683013c71a67ffbae74f75dabec9ddb439552
Yewon Sim <syw0729@naver.com>
2023-09-20 17:23:41 +0900

    tennis likelihood

--- tennis/total_likelihood.py
+++ tennis/total_likelihood.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import os
 
-file_names = ['tennis_data/Outlook_likelihood.csv', 'tennis_data/Temperature_likelihood.csv',
-              'tennis_data/Humidity_likelihood.csv', 'tennis_data/Wind_likelihood.csv']
+file_names = ['Outlook_likelihood.csv', 'Temperature_likelihood.csv',
+              'Humidity_likelihood.csv', 'Wind_likelihood.csv']
 likelihoods = []
 
 for file_name in file_names:
-    df = pd.read_csv(file_name, index_col=0)
+    df = pd.read_csv(os.path.join('tennis_data', file_name), index_col=0)
     likelihoods.append(df)
 
 total_likelihood = pd.concat(likelihoods)
