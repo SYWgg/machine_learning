0d706377e87f66093e3d902152cd27eeccf1e2ea
Yewon Sim <syw0729@naver.com>
2023-09-25 13:35:11 +0900

    dataset와 초기centroid

--- kmeans_clustering/main.py
+++ kmeans_clustering/main.py
@@ -11,3 +11,9 @@ random_indices = np.arange(400)
 np.random.shuffle(random_indices)
 random_indices = random_indices[:K]
 centroids = X[random_indices] # 실제 존재하는 데이터에서 K개를 centroid로 설정
+
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.scatter(X[:, 0], X[:, 1], alpha=0.5)
+ax.scatter(centroids[:, 0], centroids[:, 1],
+           color='red', marker='X', s=100)
+plt.show()
